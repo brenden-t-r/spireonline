9aaa3f6b3134373914899b54024108887b3e5e4c
Brenden Romanowski <brenden.romanowski@gmail.com>
2019-07-22 19:56:45 -0500

    Adding wavesurfer.js library

--- src/main/webapp/src/app/app.component.ts
+++ src/main/webapp/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import WaveSurfer from 'wavesurfer.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'webapp';
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+    var wavesurfer = WaveSurfer.create({
+      container: '#waveform',
+      waveColor: 'violet',
+      progressColor: 'purple'
+    });
+
+    wavesurfer.load('https://srv-file2.gofile.io/download/jKE1Xq/test.flac');
+
+    wavesurfer.on('ready', function () {
+      wavesurfer.play();
+    });
+  }
 }
